cbe10749d1ffd13bd18a9f635c33a4a4f4cc18af
Ray <loveyou520babytw@gmail.com>
2019-03-23 12:45:27 +0800

    大小寫修正

---
+++ src/components/pages/Breadcrumb.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  nav(aria-label='breadcrumb')
+    ol.breadcrumb.bg-transparent
+      li.breadcrumb-item
+        a(href='#') 首頁
+      li.breadcrumb-item.active(aria-current='page') 全部商品
+</template>
