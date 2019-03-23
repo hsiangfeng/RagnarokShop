e74303089ee0f824e8ab0654f1379bc0b47179f6
Ray <loveyou520babytw@gmail.com>
2019-03-23 23:03:17 +0800

    修正git大小寫問題

--- src/components/pages/breadcrumb.vue
+++ src/components/pages/breadcrumb.vue
@@ -4,12 +4,12 @@
       li.breadcrumb-item
         router-link(:to="{name: 'Index'}")
           | 首頁
-      li.breadcrumb-item(aria-current='page' v-if="breadcrumb.title")
+      li.breadcrumb-item(aria-current='page' v-if='breadcrumb.title')
           | {{breadcrumb.title}}
-      li.breadcrumb-item(aria-current='page' v-if="breadcrumb.productsList")
+      li.breadcrumb-item(aria-current='page' v-if='breadcrumb.productsList')
         router-link(:to="{name: 'Products', params:{data: '全部商品'}}")
           | {{breadcrumb.productsList}}
-      li.breadcrumb-item(aria-current='page' v-if="breadcrumb.productsTitle")
+      li.breadcrumb-item(aria-current='page' v-if='breadcrumb.productsTitle')
           | {{breadcrumb.productsTitle}}
 </template>
 
