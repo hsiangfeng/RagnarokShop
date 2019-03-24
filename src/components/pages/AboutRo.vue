2a393f2493bc7c3441452ff2b925fcddcf39712a
Ray <loveyou520babytw@gmail.com>
2019-03-24 17:54:45 +0800

    修正AboutRo 在firefox跑板問題

--- src/components/pages/AboutRo.vue
+++ src/components/pages/AboutRo.vue
@@ -21,14 +21,13 @@
             .col-md-6
               img(src="../../assets/img/about/00007711.jpg").img-fluid.about-img
             .col-md-6.d-flex.justify-content-center
-              .h-100
-                .about-content
-                  .title.text-ro-dark.h4.text-center.font-weight-bold 中央之城『普隆德拉』
-                  .about-text.text-ro-dark.font-weight-bold
-                    p
-                      | 是盧恩米德加爾特王國的中心，經過高度開發，成立「斯楚瓦爾茲貝特」共和國。
-                    p
-                      | 在城中有很多的設施如大教堂、武士公會、新手訓練中心，還有一個可以容納為數眾多玩家的大公園。
+              .about-content
+                .title.text-ro-dark.h4.text-center.font-weight-bold 中央之城『普隆德拉』
+                .about-text.text-ro-dark.font-weight-bold
+                  p
+                    | 是盧恩米德加爾特王國的中心，經過高度開發，成立「斯楚瓦爾茲貝特」共和國。
+                  p
+                    | 在城中有很多的設施如大教堂、武士公會、新手訓練中心，還有一個可以容納為數眾多玩家的大公園。
     section.margin-about-top
       .bg-ro-light.about-bg-ro
         .container
@@ -36,14 +35,13 @@
             .col-md-6
               img(src="../../assets/img/about/00007339.jpg").img-fluid.about-img
             .col-md-6.d-flex.justify-content-center
-              .h-100
-                .about-content
-                  .title.text-ro-dark.h4.text-center.font-weight-bold 魔法之都『吉芬』
-                  .about-text.text-ro-dark.font-weight-bold
-                    p
-                      | 位於的西北方，「斯楚瓦爾茲貝特」共和國的邊界上。從很久以前，「瑪娜神力」的力量吸引了許多魔法修習者，也造就了『吉芬』今日的樣貌。
-                    p
-                      | 傳說在城市的地底下有一座古老的遺跡，但並沒有人可以證實它的存在與否；唯一存在的是有關建來封印遺跡的高塔的傳奇故事。
+              .about-content
+                .title.text-ro-dark.h4.text-center.font-weight-bold 魔法之都『吉芬』
+                .about-text.text-ro-dark.font-weight-bold
+                  p
+                    | 位於的西北方，「斯楚瓦爾茲貝特」共和國的邊界上。從很久以前，「瑪娜神力」的力量吸引了許多魔法修習者，也造就了『吉芬』今日的樣貌。
+                  p
+                    | 傳說在城市的地底下有一座古老的遺跡，但並沒有人可以證實它的存在與否；唯一存在的是有關建來封印遺跡的高塔的傳奇故事。
     section.margin-about-top
       .bg-ro-light.about-bg-ro
         .container
@@ -51,16 +49,15 @@
             .col-md-6
               img(src="../../assets/img/about/00007713.jpg").img-fluid.about-img
             .col-md-6.d-flex.justify-content-center
-              .h-100
-                .about-content
-                  .title.text-ro-dark.h4.text-center.font-weight-bold 邊陲之城『夢蘿克』
-                  .about-text.text-ro-dark.font-weight-bold
-                    p
-                      | 位於『普隆德拉』的西南方，『夢蘿克』沙漠的一個綠洲裡，又稱為沙漠之城。
-                    p
-                      | 由於它強烈的地理特性，共和國政府的力量無法影響此地，無能的地方首長，使得整個城市為盜賊所支配。
-                    p
-                      | 謠傳說，羅奇，盜賊的導師，也躲藏在此城的某處。
+              .about-content
+                .title.text-ro-dark.h4.text-center.font-weight-bold 邊陲之城『夢蘿克』
+                .about-text.text-ro-dark.font-weight-bold
+                  p
+                    | 位於『普隆德拉』的西南方，『夢蘿克』沙漠的一個綠洲裡，又稱為沙漠之城。
+                  p
+                    | 由於它強烈的地理特性，共和國政府的力量無法影響此地，無能的地方首長，使得整個城市為盜賊所支配。
+                  p
+                    | 謠傳說，羅奇，盜賊的導師，也躲藏在此城的某處。
     section.margin-about-top
       .bg-ro-light.about-bg-ro
         .container
@@ -68,14 +65,13 @@
             .col-md-6
               img(src="../../assets/img/about/eLfCllc.jpg").img-fluid.about-img
             .col-md-6.d-flex.justify-content-center
-              .h-100
-                .about-content
-                  .title.text-ro-dark.h4.text-center.font-weight-bold 山中之城『斐揚』
-                  .about-text.text-ro-dark.font-weight-bold
-                    p
-                      | 是深林裡的一個小村莊，也是漫畫中主要人物愛勒絲的家鄉，與外面世界的隔絕，使得依然『斐揚』得以保留它的傳統和地方特色，並未受到盧恩─米德加爾特王國的影響。
-                    p
-                      | 在這裡修練的弓箭手，也在村後搭建了他們自己的村落。玩家可以在此進行『弓箭手』的技能修行。
+              .about-content
+                .title.text-ro-dark.h4.text-center.font-weight-bold 山中之城『斐揚』
+                .about-text.text-ro-dark.font-weight-bold
+                  p
+                    | 是深林裡的一個小村莊，也是漫畫中主要人物愛勒絲的家鄉，與外面世界的隔絕，使得依然『斐揚』得以保留它的傳統和地方特色，並未受到盧恩─米德加爾特王國的影響。
+                  p
+                    | 在這裡修練的弓箭手，也在村後搭建了他們自己的村落。玩家可以在此進行『弓箭手』的技能修行。
     section.margin-about-top
       .bg-ro-light.about-bg-ro
         .container
@@ -83,14 +79,13 @@
             .col-md-6
               img(src="../../assets/img/about/00007667.jpg").img-fluid.about-img
             .col-md-6.d-flex.justify-content-center
-              .h-100
-                .about-content
-                  .title.text-ro-dark.h4.text-center.font-weight-bold 海港之城『艾爾貝塔』
-                  .about-text.text-ro-dark.font-weight-bold
-                    p
-                      | 位於『普隆德拉』東南方的港灣，也是盧恩─米德加爾特王國的商業中心；但激變的天氣形成兇猛的暴風和海嘯，常使得船隻無法離港。
-                    p
-                      | 「斯楚瓦爾茲貝特」的寶藏沈船，使得寶物獵人和商人大量聚集於此。以商人為職業的玩家須在此地修練。
+              .about-content
+                .title.text-ro-dark.h4.text-center.font-weight-bold 海港之城『艾爾貝塔』
+                .about-text.text-ro-dark.font-weight-bold
+                  p
+                    | 位於『普隆德拉』東南方的港灣，也是盧恩─米德加爾特王國的商業中心；但激變的天氣形成兇猛的暴風和海嘯，常使得船隻無法離港。
+                  p
+                    | 「斯楚瓦爾茲貝特」的寶藏沈船，使得寶物獵人和商人大量聚集於此。以商人為職業的玩家須在此地修練。
     audio#roBGM(loop='', muted='', webkit-playsinline='true', playsinline='true')
       source(src='https://raw.githubusercontent.com/hsiangfeng/RagnarokShop/gh-pages/static/12.mp3', type='audio/mpeg')
 </template>
@@ -122,6 +117,7 @@
   }
   .about-content{
     writing-mode: vertical-rl;
+    max-height: 300px;
     @media (max-width:768px) {
       writing-mode: unset;
     }
